c0d5cfbfad2a2fbdb13d6f584d1c97f7b091f5e8
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-24 23:30:48 +0300

    login

--- front/src/Components/Login/index.js
+++ front/src/Components/Login/index.js
@@ -1,4 +1,116 @@
-import './style.css';
+
+import { Form, Button, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [logResponse, setLogResponse] = useState("");
+
+  const navigate = useNavigate();
+
+  const login = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://sweetspot-8-back.herokuapp.com/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data.status == true) {
+          console.log("its true");
+          navigate("/", { state: { userName: response.data.name } });
+        }
+
+        if (response.data.status == false) {
+          console.log("false body");
+          setLogResponse(response.data.respLog);
+          navigate("/login");
+        }
+      });
+  };
+  const goReg = () => {
+    navigate("/register");
+  };
+  return (
+    <Container>
+      <h1 className="text-center p-5 bg-dark text-white">Login</h1>
+      <form action="/login" method="POST">
+        <Form.Group className="m-5" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+          />
+        </Form.Group>
+
+        <Form.Group className="mt-5 mx-5" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => {
+              setpassword(e.target.value);
+            }}
+          />
+        </Form.Group>
+        <Form.Group className="mx-5 mt-2" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Remeber me" />
+        </Form.Group>
+        <p>{logResponse}</p>
+
+        <Button className="btn btn-dark m-5 px-4 py-2" onClick={login}>
+          Login
+        </Button>
+        <Button
+          type="button"
+          className="m-0 px-4 py-2"
+          variant="outline-dark"
+          onClick={goReg}
+        >
+          Register
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default Login;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import './style.css';
 import React from 'react';
 import {FormControl,FormGroup,Form,Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -36,3 +148,5 @@ const Login=()=> {
 }
 
 export default Login;
+
+*/
--- front/src/Components/Registration/index.js
+++ front/src/Components/Registration/index.js
@@ -1,4 +1,120 @@
- import React from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Register = () => {
+  const [usernameReg, setusernameReg] = useState("");
+  const [emailReg, setEmailReg] = useState("");
+  const [passwordReg, setpasswordReg] = useState("");
+  const navigate = useNavigate();
+  //   const [currentUser, setcurrentUser] = useState("");
+  const [regResponse, setregResponse] = useState("");
+
+  const reg = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://sweetspot-8-back.herokuapp.com/register", {
+        name: usernameReg,
+        email: emailReg,
+        password: passwordReg,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data.status == true) {
+          //   setcurrentUser(response.data.name);
+          console.log("registered");
+          navigate("/", { state: {userName:response.data.name} }); //lets assume home is homepage and pass currentUser
+        }
+
+        if (response.data.status == false) {
+          setregResponse(response.data.resp);
+          navigate("/register");
+        }
+      });
+  };
+
+  const goLogin = () => {
+    navigate("/login");
+  };
+  return (
+    <Container>
+      <h1 className="text-center p-5 bg-dark text-white">Register</h1>
+      <form action="/movies/register" method="POST">
+        <Form.Group className="mt-5 mx-5" controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+
+          <Form.Control
+            type="text"
+            name="name"
+            required
+            onChange={(e) => {
+              setusernameReg(e.target.value);
+            }}
+            placeholder="Enter Username"
+          />
+        </Form.Group>
+        <Form.Group className="m-5" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            required
+            onChange={(e) => {
+              setEmailReg(e.target.value);
+            }}
+          />
+        </Form.Group>
+
+        <Form.Group className="mt-5 mx-5" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            name="email"
+            required
+            onChange={(e) => {
+              setpasswordReg(e.target.value);
+            }}
+          />
+        </Form.Group>
+
+        <p className="mx-5 mt-4 text-danger ">{regResponse}</p>
+        <Button
+          className="btn btn-dark m-5 px-4 py-2"
+          type="submit"
+          onClick={reg}
+        >
+          Register
+        </Button>
+        <Button
+          type="button"
+          className="m-0 px-4 py-2"
+          variant="outline-dark"
+          onClick={goLogin}
+        >
+          Login
+        </Button>
+      </form>
+    </Container>
+  );
+};
+
+export default Register; 
+ 
+ 
+ 
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ /*import React from "react";
 
 import './style.css';
 
@@ -18,4 +134,4 @@ const Registration =()=>{
   );
 }
 
-export default Registration ;
+export default Registration ;*/
